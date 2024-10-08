9ded76e26c4c6e92a120bf16e37bd445a939de48
Shreyansh-32 <thakurshreyansh0609@gmail.com>
2024-10-08 21:06:15 +0530

    Added .env to .gitignore

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,116 +1,158 @@
-const {Router} = require('express');
-const bcrypt = require('bcrypt');
-const { adminModel, productModel } = require('../db');
+const { Router } = require("express");
+const bcrypt = require("bcrypt");
+const { adminModel, productModel } = require("../db");
 const jwt = require("jsonwebtoken");
-require('dotenv').config();
-const { ObjectId } = require('mongodb');
-const { adminMiddleware } = require('../middlewares/admin');
+require("dotenv").config();
+const { ObjectId } = require("mongodb");
+const { adminMiddleware } = require("../middlewares/admin");
+const zod = require("zod");
 
 const adminRouter = Router();
 
-adminRouter.post('/signup' , async(req , res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const phone = req.body.phone;
+adminRouter.post("/signup", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const phone = req.body.phone;
 
-    try{
-        const hashedPassword = await bcrypt.hash(password , 5);
-        await adminModel.create({
-            email : email,
-            password : hashedPassword,
-            firstName : firstName,
-            lastName : lastName,
-            phone : phone
-        });
-        res.status(200).json({
-            "message" : "Signed up successfully"
-        })
-    }
-    catch(err){
-        res.status(500).json({
-            "message" : "Internal server error"
-        });
-    }
-});
-
-adminRouter.post("/signin", async(req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-  
-    try{
-
-        const admin = await adminModel.findOne({email});
-
-        if(admin){
-
-            const comparePassword = await bcrypt.compare(password , admin.password);
-
-            if(comparePassword){
-                const token = jwt.sign({id : admin._id} , process.env.JWT_ADMIN_PASSWORD);
-                res.status(200).json({
-                    "message" : "Signed in successfully",
-                    token : token
-                });
-            }
-            else{
-                res.status(403).json({
-                    "message" : "Incorrect password"
-                });
-            }
-        }
-        else{
-            res.status(403).json({
-                "message" : "Admin with given email doesn't exist"
-            });
-        }
-    }
-    catch(err){
-        res.status(500).json({
-            "message" : "Internal server error"
-        });
-    }
+  const Schema = zod.object({
+    email: zod.string().email(),
+    password: zod.string(),
+    firstName: zod.string(),
+    lastName: zod.string(),
+    phone: zod.number(),
   });
 
-adminRouter.post('/addproduct' , adminMiddleware ,async(req , res) => {
-    const {title , description , writer , page , price , seller , quantity , imageUrl} = req.body;
+  try {
+    const response = Schema.safeParse({
+      email,
+      password,
+      firstName,
+      lastName,
+      phone,
+    });
 
-    try{
-        const prod = await productModel.findOne({title , writer , seller : new ObjectId(seller)});
-        console.log(prod);
-        if(prod){
-            await productModel.updateOne({title , writer , seller : new ObjectId(seller)} , {
-                quantity : prod.quantity + quantity
-            });
-            res.status(200).json({
-                "message" : "Product updated successfully"
-            });
-        }
-        else{
-            await productModel.create({
-                title,
-                description,
-                writer,
-                page,
-                price,
-                seller:new ObjectId(seller),
-                quantity,
-                imageUrl
-            });
-    
-            res.status(200).json({
-                "message" : "Product added successfully"
-            });
-        }
-    }
-    catch(err){
-        res.status(500).json({
-            "message" : "Internal server error"
+    if (response.success) {
+      const admin = adminModel.findOne({ email });
+      if (!admin) {
+        const hashedPassword = await bcrypt.hash(password, 5);
+        await adminModel.create({
+          email: email,
+          password: hashedPassword,
+          firstName: firstName,
+          lastName: lastName,
+          phone: phone,
         });
+        res.status(200).json({
+          message: "Signed up successfully",
+        });
+      } else {
+        res.status(403).json({
+          message: "Admin with given email exist",
+        });
+      }
+    } else {
+      res.status(403).json({
+        message: "Invalid input details",
+        error: response.error.issues,
+      });
     }
-})
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
+
+adminRouter.post("/signin", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const admin = await adminModel.findOne({ email });
+
+    if (admin) {
+      const comparePassword = await bcrypt.compare(password, admin.password);
+
+      if (comparePassword) {
+        const token = jwt.sign(
+          { id: admin._id },
+          process.env.JWT_ADMIN_PASSWORD
+        );
+        res.status(200).json({
+          message: "Signed in successfully",
+          token: token,
+        });
+      } else {
+        res.status(403).json({
+          message: "Incorrect password",
+        });
+      }
+    } else {
+      res.status(403).json({
+        message: "Admin with given email doesn't exist",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
+
+adminRouter.post("/addproduct", adminMiddleware, async (req, res) => {
+  const {
+    title,
+    description,
+    writer,
+    page,
+    price,
+    seller,
+    quantity,
+    imageUrl,
+  } = req.body;
+
+  try {
+    const prod = await productModel.findOne({
+      title,
+      writer,
+      seller: new ObjectId(seller),
+    });
+    console.log(prod);
+    if (prod) {
+      await productModel.updateOne(
+        { title, writer, seller: new ObjectId(seller) },
+        {
+          quantity: prod.quantity + quantity,
+        }
+      );
+      res.status(200).json({
+        message: "Product updated successfully",
+      });
+    } else {
+      await productModel.create({
+        title,
+        description,
+        writer,
+        page,
+        price,
+        seller: new ObjectId(seller),
+        quantity,
+        imageUrl,
+      });
+
+      res.status(200).json({
+        message: "Product added successfully",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
 
 module.exports = {
-    adminRouter : adminRouter
-}
+  adminRouter: adminRouter,
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const bcrypt = require("bcrypt");
-const { userModel, cartModel, productModel , ordersModel } = require("../db");
+const { userModel, cartModel, productModel, ordersModel } = require("../db");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { ObjectId } = require("mongodb");
 const { userMiddleware } = require("../middlewares/user");
+const zod = require("zod");
 
 const userRouter = Router();
 
@@ -12,25 +13,42 @@ userRouter.post("/signin", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  try {
-    const user = await userModel.findOne({ email });
+  const Shema = zod.object({
+    email: zod.string().email(),
+    password: zod.string(),
+  });
 
-    if (user) {
-      const comparePassword = await bcrypt.compare(password, user.password);
-      if (comparePassword) {
-        const token = jwt.sign({id :user._id}, process.env.JWT_USER_PASSWORD);
-        res.status(200).json({
-          message: "Singed in successfully",
-          token: token,
-        });
+  try {
+    const response = Shema.safeParse({ email, password });
+
+    if (response.success) {
+      const user = await userModel.findOne({ email });
+
+      if (user) {
+        const comparePassword = await bcrypt.compare(password, user.password);
+        if (comparePassword) {
+          const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_USER_PASSWORD
+          );
+          res.status(200).json({
+            message: "Singed in successfully",
+            token: token,
+          });
+        } else {
+          res.status(403).json({
+            message: "Incorrect Credentials",
+          });
+        }
       } else {
         res.status(403).json({
-          message: "Incorrect Credentials",
+          message: "User with given email doesn't exist",
         });
       }
     } else {
       res.status(403).json({
-        message: "User with given email doesn't exist",
+        message: "Incorrect input format",
+        error: response.error.issues,
       });
     }
   } catch (err) {
@@ -47,18 +65,50 @@ userRouter.post("/signup", async (req, res) => {
   const lastName = req.body.lastName;
   const phone = req.body.phone;
 
+  const Schema = zod.object({
+    email: zod.string().email(),
+    password: zod.string(),
+    firstName: zod.string(),
+    lastName: zod.string(),
+    phone: zod.number(),
+  });
+
   try {
-    const hashedPassword = await bcrypt.hash(password, 5);
-    await userModel.create({
-      email: email,
-      password: hashedPassword,
-      firstName: firstName,
-      lastName: lastName,
-      phone: phone,
+    const response = Schema.safeParse({
+      email,
+      password,
+      firstName,
+      lastName,
+      phone,
     });
-    res.status(200).json({
-      message: "Signed up successfully"
-    });
+
+    if (response.success) {
+      const user = userModel.findOne({email});
+
+      if(!user){
+        const hashedPassword = await bcrypt.hash(password, 5);
+      await userModel.create({
+        email: email,
+        password: hashedPassword,
+        firstName: firstName,
+        lastName: lastName,
+        phone: phone,
+      });
+      res.status(200).json({
+        message: "Signed up successfully",
+      });
+      }
+      else{
+        res.status(403).json({
+          "message" : "User with given email exist"
+        });
+      }
+    } else {
+      res.status(403).json({
+        message: "Invalid input type",
+        error: response.error.issues,
+      });
+    }
   } catch (err) {
     res.status(500).json({
       message: "Internal server error",
@@ -66,32 +116,35 @@ userRouter.post("/signup", async (req, res) => {
   }
 });
 
-userRouter.post("/addcart" , userMiddleware , async (req, res) => {
+userRouter.post("/addcart", userMiddleware, async (req, res) => {
   const { productId, userId, quantity } = req.body;
 
   try {
-    const prod = await cartModel.findOne({productId : new ObjectId(productId)})
+    const prod = await cartModel.findOne({
+      productId: new ObjectId(productId),
+    });
 
-    if(prod){
-        await cartModel.updateOne({productId : new ObjectId(productId)} , {
-            quantity : prod.quantity+quantity
-        });
+    if (prod) {
+      await cartModel.updateOne(
+        { productId: new ObjectId(productId) },
+        {
+          quantity: prod.quantity + quantity,
+        }
+      );
 
-        res.status(200).json({
-            "message" : "Updated cart successfully"
-        });
-    }
-    else{
-        await cartModel.create({
-            productId: new ObjectId(productId),
-            userId: new ObjectId(userId),
-            quantity,
-          });
-      
-          
-          res.status(200).json({
-              "message" : "Added to cart successfully"
-          });
+      res.status(200).json({
+        message: "Updated cart successfully",
+      });
+    } else {
+      await cartModel.create({
+        productId: new ObjectId(productId),
+        userId: new ObjectId(userId),
+        quantity,
+      });
+
+      res.status(200).json({
+        message: "Added to cart successfully",
+      });
     }
   } catch (err) {
     res.status(500).json({
@@ -100,38 +153,36 @@ userRouter.post("/addcart" , userMiddleware , async (req, res) => {
   }
 });
 
-userRouter.post('/orders' , userMiddleware , async(req , res) => {
-    const {productId , userId , quantity} = req.body;
-    try{
-        console.log(new Date().toLocaleTimeString());
-        await ordersModel.create({
-            productId : new ObjectId(productId),
-            userId : new ObjectId(userId),
-            quantity : quantity,
-            time : new Date().toLocaleTimeString(),
-            date:new Date().toLocaleDateString()
-        });
-        console.log("Order placed");
+userRouter.post("/orders", userMiddleware, async (req, res) => {
+  const { productId, userId, quantity } = req.body;
+  try {
+    console.log(new Date().toLocaleTimeString());
+    await ordersModel.create({
+      productId: new ObjectId(productId),
+      userId: new ObjectId(userId),
+      quantity: quantity,
+      time: new Date().toLocaleTimeString(),
+      date: new Date().toLocaleDateString(),
+    });
+    console.log("Order placed");
 
-        const prod = await productModel.findOne({
-            _id: new ObjectId(productId),
-        });
-        
-            
-        await productModel.updateOne(
-          { _id: new ObjectId(productId) },
-          { $set: { quantity: prod.quantity - quantity } } 
-        );
+    const prod = await productModel.findOne({
+      _id: new ObjectId(productId),
+    });
 
-        res.status(200).json({
-            "message" : "Order placed successfully"
-        });
-    }
-    catch(err){
-        res.status(500).json({
-            "message" : "Internal server error"
-        });
-    }
+    await productModel.updateOne(
+      { _id: new ObjectId(productId) },
+      { $set: { quantity: prod.quantity - quantity } }
+    );
+
+    res.status(200).json({
+      message: "Order placed successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 });
 
 module.exports = {
