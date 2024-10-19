4c356cb7b3693c845b07a375acd8cd5371e1c6ba
Shreyansh-32 <thakurshreyansh0609@gmail.com>
2024-10-20 01:27:14 +0530

    Made changes to cart and orders router

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,9 +1,23 @@
 const {Router} = require('express');
+const { userMiddleware } = require('../middlewares/user');
 
 const cartRouter = Router();
 
-cartRouter.get('/' , (req , res) => {
+cartRouter.get('/' , userMiddleware , async(req , res) => {
+    const userId = req.body.userId;
 
+    try{
+        const cart = await cartModel.find({userId : new ObjectId(userId)});
+        res.status(200).json({
+            "message" : "Successfully fetched cart",
+            "cart" : cart
+        });
+    }
+    catch(err){
+        res.status(500).json({
+            "message" : "Internal server error"
+        });
+    }
 });
 
 module.exports = {
--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,10 +1,26 @@
 const {Router} = require('express');
 const { productModel, ordersModel } = require('../db');
 const { ObjectId } = require("mongodb");
+const { userMiddleware } = require('../middlewares/user');
 
 const ordersRouter = Router();
 
+ordersRouter.get('/' , userMiddleware , async(req , res) => {
+    const userId = req.body.userId;
 
+    try{
+        const orders = await ordersModel.find({userId : new ObjectId(userId)});
+        res.status(200).json({
+            "message" : "Successfully fetched orders",
+            "orders" : orders
+        });
+    }
+    catch(err){
+        res.status(500).json({
+            "message" : "Internal server error"
+        });
+    }
+});
 
 module.exports = {
     ordersRouter : ordersRouter
